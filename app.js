abafed8ef35cbf512946a3844cc80c80b1a7fe1d
ajdin <ajdinhusetovic@outlook.com>
2023-01-01 17:54:09 +0100

    addded basic functionality

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const section = document.querySelector('.game');
+let click = true;
 
-const createBoard = () => {
-    for (let i = 0; i < 256; i++){
+const game = () => {
+    // creating game board
+    for (let i = 0; i < 1024; i++){
         const div = document.createElement('div');
         div.classList.add('board');
         section.appendChild(div);
+        // when mouse hovers over divs they change colors ("drawing" effect)
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "purple";
+        })
     }
 }
 
-createBoard();
+
+game();
