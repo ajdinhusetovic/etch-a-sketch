397ff13680e671391cc2c1def4177d1bf03950eb
ajdin <ajdinhusetovic@outlook.com>
2023-01-02 17:43:46 +0100

    added function that can change board size and added the ability to change color

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const board = document.querySelector('.board');
-let size = 16;
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+const boardSizeInput = document.querySelector('.board-size');
+const boardSizeChangeBtn = document.querySelector('.board-size-btn');
 
-for (let i = 0; i < size * size; i++){
-    let square = document.createElement('div');
-    square.style.backgroundColor = "blue";
-    board.appendChild(square)
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "black";
-    })
+
+
+function makeBoard(size){
+    const board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size * size; i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = "white";
+        board.appendChild(square)
+        square.addEventListener('mouseover', colorSquare)
+    }
+}
+
+makeBoard(16);
+
+function changeSize(input){
+    if (input > 100 || input < 2){
+        alert("INVALID SIZE (2 is minimum and 100 is maximum)")
+    }else {
+        makeBoard(input);
+    }
+}
+
+function colorSquare(){
+    this.style.backgroundColor = color;
+}
+
+function changeColor(c){
+    color = c
 }
